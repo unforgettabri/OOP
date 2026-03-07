5ea96ddc0c33da0c20cc54417fc883000f096934
unforgettabri <brianna.bustamante@lorma.edu>
2026-03-07 13:36:38 +0000

    tapos na sir

--- activity8/Main.java
+++ activity8/Main.java
@@ -1,5 +1,13 @@
 public class Main {
-    MyRectangle2D r = new MyRectangle2D(2, 2, 3, 5.5);
-    MyRectangle2D r1 = new MyRectangle2D(3, 3, 2, 1);
-    MyRectangle2D r2 = new MyRectangle2D(0, 0, 2, 6);
+    public static void main(String[] args) {
+        MyRectangle2D r = new MyRectangle2D(2, 2, 3, 5.5);
+        MyRectangle2D r1 = new MyRectangle2D(3, 3, 2, 1);
+        MyRectangle2D r2 = new MyRectangle2D(0, 0, 2, 6);
+
+        System.out.println("Area of r: " + r.getArea());
+        System.out.println("Perimeter of r: " + r.getPerimeter());
+
+        System.out.println("r contains r1: " + r.contains(r1));
+        System.out.println("r overlaps r2: " + r.overlaps(r2));
+    }
 }
--- activity8/MyRectangle2D.java
+++ activity8/MyRectangle2D.java
@@ -43,7 +43,7 @@ public class MyRectangle2D {
         this.x = 0;
         this.y = 0;
         this.width = 1;
-        this.width = 1;
+        this.height = 1;
     }
 
     // constructor(specified)
@@ -64,18 +64,19 @@ public class MyRectangle2D {
         return 2 * (width + height);
     }
 
+    // contains: include edges
     public boolean contains(MyRectangle2D r) {
-        // return true if the specified rectangle is inside this rectangle
-        if ((r.x + r.width < this.x + this.width) && (r.x > this.x) && (r.y + r.height < this.y + this.height)
-                && (r.y > this.y)) {
-            return true;
-        } else {
-            return false;
-        }
+        return r.x >= this.x &&
+            r.y >= this.y &&
+            r.x + r.width <= this.x + this.width &&
+            r.y + r.height <= this.y + this.height;
     }
 
+    // overlaps: proper check
     public boolean overlaps(MyRectangle2D r) {
-        // di ko na ammo aramidek
-        return true;
+        return !(r.x + r.width < this.x ||   // r completely left
+                r.x > this.x + this.width || // r completely right
+                r.y + r.height < this.y ||   // r completely below
+                r.y > this.y + this.height); // r completely above
     }
 }
